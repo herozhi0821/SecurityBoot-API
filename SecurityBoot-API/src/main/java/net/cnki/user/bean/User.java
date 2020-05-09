6886936d6809e50299018af90ff703baddbd6e01
herozhi0821 <ZhiPengyu@ZhiPengyu>
2020-05-09 09:03:50 +0800

    null

---
+++ SecurityBoot-API/src/main/java/net/cnki/user/bean/User.java
@@ -0,0 +1,76 @@
+package net.cnki.user.bean;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails{
+
+	private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String headPortrait;
+
+    private String nickname;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
+
+    private Boolean enabled;
+
+    private String validTime;
+
+    private String updateTime;
+
+    private String remark;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return enabled;
+	}
+
+}
