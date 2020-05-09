9fd0894b2c7f611088d24bb7765f89fdd14e7d83
herozhi0821 <ZhiPengyu@ZhiPengyu>
2020-05-09 09:19:07 +0800

    调试新的用户策略

--- SecurityBoot-API/src/main/java/net/cnki/user/bean/User.java
+++ SecurityBoot-API/src/main/java/net/cnki/user/bean/User.java
@@ -7,11 +7,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public class User implements UserDetails{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2529883455226314998L;
+
 	private Integer id;
-
-    private String username;
-
-    private String password;
 
     private String headPortrait;
 
@@ -31,6 +32,7 @@ public class User implements UserDetails{
 
     private String remark;
 
+    
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
@@ -71,6 +73,92 @@ public class User implements UserDetails{
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
 		return enabled;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getHeadPortrait() {
+		return headPortrait;
+	}
+
+	public void setHeadPortrait(String headPortrait) {
+		this.headPortrait = headPortrait;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getValidTime() {
+		return validTime;
+	}
+
+	public void setValidTime(String validTime) {
+		this.validTime = validTime;
+	}
+
+	public String getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public void setUsername(String username) {
+	}
+
+	public void setPassword(String password) {
 	}
 
 }
